a36b1d77083ec49b7235dfb832eade07b87fcc51
Umar Baig <Umarbaig2012.ub@gmail.com>
2019-05-10 17:57:18 +0500

    Backend using node js and express

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email:{
+      type:String,
+      required:[true,'Email is required'],
+      unique:true
+  },
+  fName:{
+      type:String,
+      rrequired:[true,'Full Name is required']
+  },
+  profilePic:{
+      type:String
+  },
+  firebaseUID:{
+      type:String
+  },
+  isLoggedIn:{
+      type:Boolean,
+      default:false
+  },
+  createdDate:{
+      type:Date,
+      default:Date.now()
+  },
+  tokens:{
+      type:[String]
+  }
+
+});
+
+module.exports = mongoose.model('Users', UserSchema);
